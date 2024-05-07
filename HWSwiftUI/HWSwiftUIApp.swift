cac9fde9d502eedd1e3c4a50a44c413354499b03
Rad <radoslaw.chomik@bnpparibas-pf.co.uk>
2024-05-07 12:58:53 +0100

    Initial Commit

---
+++ HWSwiftUI/HWSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSwiftUIApp.swift
+//  HWSwiftUI
+//
+//  Created by Radoslaw Chomik on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HWSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
